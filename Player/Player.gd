3c8620d75a8c302ecf41dd20e645541660f09847
Ernesto Torres <ernesto.torres.d@gmail.com>
2021-04-12 20:12:24 -0500

    Place corn with click

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,13 @@
 extends KinematicBody
 
 export var mouse_sensitivity = 0.5
+export var ray_length = 200.0
 
 onready var camera = $Camera
 onready var move_component = $MoveComponent
+onready var raycast = $RayCast
+
+var corn = preload("res://Corn/CornPickUp.tscn")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -29,9 +33,33 @@ func _process(_delta):
 	if Input.is_action_just_pressed("jump"):
 		move_component.jump()
 	
+#	if Input.is_action_just_pressed("shoot"):
+#		print("shooting")
+#		var from = camera.project_ray_origin()
+#		var col = raycast.get_collider()
+#		if raycast.is_colliding():
+#			print("colliding")
+			
+		
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= mouse_sensitivity * event.relative.x
 		camera.rotation_degrees.x -= mouse_sensitivity * event.relative.y
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
+	
+	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
+		var camera = $Camera
+		var from = camera.project_ray_origin(event.position)
+		var to = from + camera.project_ray_normal(event.position) * ray_length
+		
+		var space_state = get_world().get_direct_space_state()
+		var result = space_state.intersect_ray(from, to, [], 1, true, true)
+		if result:
+			print(result)
+			var corn_instance = corn.instance();
+			get_tree().get_root().add_child(corn_instance)
+			corn_instance.global_transform.origin = result.position
+			
+		
+		
 		
