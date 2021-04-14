b40a91df352631465324d1a5c8653e6632c2f3fb
Ernesto Torres <ernesto.torres.d@gmail.com>
2021-04-13 23:49:54 -0500

    chicken rotate + path

--- Chicken/Chicken.gd
+++ Chicken/Chicken.gd
@@ -2,9 +2,13 @@ extends Spatial
 
 export var sight_angle = 90
 export var sight_distance = 2 
+export var turn_speed = 60
 
 onready var animation_player = $chicken/AnimationPlayer
 onready var question_mark = $question_mark
+onready var move_component = $MoveComponent
+onready var collision_shape = $CollisionShape
+onready var nav: Navigation = get_tree().get_nodes_in_group("nav")[0]
 
 enum STATES { IDLE, WALK, LOOK, TRAPPED }
 
@@ -12,15 +16,19 @@ var current_state = STATES.IDLE
 var player_ref = null
 var all_corn = null
 var closest_corn = null
-
+var path = []
 
 func _ready():
-	set_state_idle()
+	set_state_walk()
 	player_ref = get_tree().get_nodes_in_group("player")[0]
+	all_corn = get_tree().get_nodes_in_group("corn")
+	set_closest_corn(all_corn)
+	print(closest_corn)
+	move_component.init(self)
 
 
 func _process(delta):
-	print(can_see_player())
+	can_see_player()
 	match current_state:
 		STATES.IDLE:
 			idle(delta)
@@ -35,33 +43,43 @@ func idle(delta):
 	pass
 
 func walk(delta):
-	pass
+	if closest_corn:
+		var current_position = global_transform.origin
+		var corn_position = closest_corn.global_transform.origin
+		path = nav.get_simple_path(current_position, corn_position)
+		
+		var dir = corn_position - current_position
+		dir.y = 0
+		move_component.set_movement_vector_as_normalized(dir)
+		
+		face_dir(corn_position, delta)
+	else:
+		# todo: rotate randomly?
+		set_state_look()
+	
 	
 func look(delta):
 	pass
 	
 func trapped(delta):
-	pass
+	move_component.freeze()
+	collision_shape.disabled = true
 
 func set_state_idle():
 	current_state = STATES.IDLE
 	animation_player.play("Idle_loop")
-	pass
 
 func set_state_walk():
 	current_state = STATES.WALK
 	animation_player.play("walk_loop")
-	pass
 
 func set_state_look():
 	current_state = STATES.LOOK
 	animation_player.play("look")
-	pass
 
 func set_state_trapped():
 	current_state = STATES.TRAPPED
 	animation_player.play("Idle_loop")
-	pass
 
 func can_see_player():
 	var direction_to_player = global_transform.origin.direction_to(player_ref.global_transform.origin)
@@ -89,6 +107,31 @@ func has_los_player():
 			return false
 	else:
 		return false
-
-
+		
+func set_closest_corn(all_corn):
+	var current_position = global_transform.origin + Vector3.UP
+	var closest = null
+	var best_distance = 99999999.0
+	for c in all_corn:
+		var corn_position = c.global_transform.origin + Vector3.UP
+		var distance = current_position.distance_squared_to(corn_position)
+		if distance < best_distance:
+			closest = c
+			best_distance = distance
+			
+		print(distance)
+		
+	closest_corn = closest
+	
+func face_dir(dir: Vector3, delta):
+	var angle_diff = global_transform.basis.z.angle_to(dir)
+	var turn_right = sign(global_transform.basis.x.dot(dir))
+	if abs(angle_diff) < deg2rad(turn_speed) * delta:
+		rotation.y = atan2(dir.x, dir.z)
+	else:
+		rotation.y += deg2rad(turn_speed) * delta * turn_right
+	
+	
+	
+	
 	
