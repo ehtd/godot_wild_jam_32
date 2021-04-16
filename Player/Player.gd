929e30c0a2e866e7db000529bf7b3d7238064527
Ernesto Torres <ernesto.torres.d@gmail.com>
2021-04-16 00:33:49 -0500

    reload corn from storage

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,12 +5,13 @@ class_name Player
 export var mouse_sensitivity = 0.5
 export var ray_length_corn = 200.0
 export var ray_length_chicken = 2.0
-export var max_corn_count = 999
+export var max_corn_count = 20
 
 onready var camera = $Camera
 onready var move_component = $MoveComponent
 onready var raycast = $RayCast
 onready var fps_label = $CanvasLayer/Label
+onready var corn_count_label = $CanvasLayer/corn_count_label
 onready var crosshair: TextureRect = $CanvasLayer/TextureRect
 
 var crosshair_normal_tex = null
@@ -21,12 +22,16 @@ signal place_corn
 var corn = preload("res://Corn/CornPickUp.tscn")
 var corn_count = 0
 
+var storage = null
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	move_component.init(self)
 	corn_count = max_corn_count
 	crosshair_normal_tex = load("res://crosshair.png")
 	crosshair_grab_tex = load("res://crosshair_selected.png")
+	storage = get_tree().get_nodes_in_group("storage")[0]
+	print(storage.get_instance_id())
 
 
 func _process(_delta):
@@ -50,9 +55,9 @@ func _process(_delta):
 	move_component.set_movement_vector_as_normalized(move_vector)
 	
 	if Input.is_action_just_pressed("grab"):
-		var result = can_grab_chicken()["result"]
+		var result = can_grab_storage()["result"]
 		if result:
-			pass
+			corn_count = max_corn_count
 
 		
 		
@@ -63,23 +68,31 @@ func _input(event):
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
 	
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
-		var result = get_ray_intersected_dictionary(event.position, 1, ray_length_corn)
+		var result = get_ray_intersected_dictionary(event.position, 1, ray_length_corn, [])
 		if result:
-			print(result)
-			add_corn_to_position(result.position)
+			var id_to_check = result.collider.get_owner().get_instance_id()
+			if id_to_check != storage.get_instance_id():
+				add_corn_to_position(result.position)
 
 func update_ui():
 	fps_label.text = str(Engine.get_frames_per_second())
-	
-	if can_grab_chicken()["can_grab"]:
+	corn_count_label.text = str(corn_count)
+	if can_grab_item(4+64)["can_grab"]:
 		set_crosshair_grab()
 	else:
 		set_crosshair_normal()
-	
+
 func can_grab_chicken():
 	var chicken_mask = 4
+	return can_grab_item(chicken_mask)
+
+func can_grab_storage():
+	var storage_mask = 64
+	return can_grab_item(storage_mask)
+	
+func can_grab_item(mask: int):
 	var mouse_pos = get_viewport().get_mouse_position()
-	var result = get_ray_intersected_dictionary(mouse_pos, chicken_mask, ray_length_chicken)
+	var result = get_ray_intersected_dictionary(mouse_pos, mask, ray_length_chicken, [])
 	var can_grab = false
 	
 	if result:
@@ -90,12 +103,12 @@ func can_grab_chicken():
 		"result": result
 	}
 	
-func get_ray_intersected_dictionary(coordinates: Vector2, collision_mask: int, ray_length):
+func get_ray_intersected_dictionary(coordinates: Vector2, collision_mask: int, ray_length, exclude: Array):
 	var camera = $Camera
 	var from = camera.project_ray_origin(coordinates)
 	var to = from + camera.project_ray_normal(coordinates) * ray_length
 	var space_state = get_world().get_direct_space_state()
-	var result = space_state.intersect_ray(from, to, [], collision_mask, true, true)
+	var result = space_state.intersect_ray(from, to, exclude, collision_mask, true, true)
 	return result
 		
 func add_corn_to_position(position: Vector3):
